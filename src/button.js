edcab17407b2bff6b944c4f56bedeb295496ce6f
Thomas Pantioras <t.pantioras@gmail.com>
2020-02-25 14:24:42 +0000

    update

---
+++ src/button.js
@@ -0,0 +1,15 @@
+// take a str, the button label and return an element
+// module.exports = buttonName => {
+//   return `Button: ${buttonName}`;
+// };
+/**
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+  return `Button: ${buttonName}`;
+};
+
+// modules.exports = makeButton;
+export default { makeButton };
