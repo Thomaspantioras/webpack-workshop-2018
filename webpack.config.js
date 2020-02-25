1e3ec9d06865bc7b6080bb75cdb54c09a8ecafa8
Thomas Pantioras <t.pantioras@gmail.com>
2020-02-25 16:06:46 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  mode: "none"
+// module.exports = env => {
+//   console.log(env);
+//   return {
+//     mode: env.mode,
+//     output: {
+//       filename: "bundle.js"
+//     }
+//   };
+// };
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = ({ mode }) => {
+  console.log(mode);
+  return {
+    mode,
+    output: {
+      filename: "bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+  };
 };
