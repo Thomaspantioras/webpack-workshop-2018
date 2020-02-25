cd552dc1a0cfc23b5f95f559ec48a445cbff010c
Thomas Pantioras <t.pantioras@gmail.com>
2020-02-25 16:45:20 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import nav from "./nav";
-import { top, bottom } from "./footer";
+import { top, bottom, footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+
+const button = makeButton("A button");
+button.style = makeColorStyle("cyan");
+document.body.appendChild(button);
+document.body.appendChild(footer);
 
 console.log(
   nav(),
   top,
   bottom,
-  makeButton("My first button!"),
+  // makeButton("My first button!"),
   makeColorStyle("cyan")
 );
