7a988c7aaaf0d0df20a39440f241b72ae42729bd
Thomas Pantioras <t.pantioras@gmail.com>
2020-02-26 13:11:39 +0000

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import css from "./footer.css";
+import buttonStyle from "./button.css";
 
 const button = makeButton("Yay! A Button!");
-button.style = makeColorStyle("cyan");
+button.style = makeColorStyle("blue");
 document.body.appendChild(button);
 document.body.appendChild(footer);
