4b3bdf6866d14448889d565ef1d9239ea936957d
Thomas Pantioras <t.pantioras@gmail.com>
2020-02-26 09:29:32 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,11 @@
 // };
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const webpackMerge = require("webpack-merge");
 
-module.exports = ({ mode }) => {
+// module.exports = ({ mode }) => {
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
   console.log(mode);
   return {
     mode,
